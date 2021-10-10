2c22055c5cd1bde283b0440dd485bec691f4f80f
Spen <69215413+DevSpen@users.noreply.github.com>
2021-10-10 10:16:49 -0400

    Added argCount

---
+++ src/functions/argCount.js
@@ -0,0 +1,31 @@
+const func = {
+    name: "$argCount",
+    description: "Returns how many arguments are in the provided text.",
+    brackets: true,
+ execute: async (d, fn) => {
+    const data = await fn.resolveArray(d)
+      let str = data[0]
+      let strlength = str.length
+
+    if (strlength === 0) {
+      let result = "0"
+        return fn.resolve(
+          result
+      )
+} else {
+      let result = str.split(" ").length
+        return fn.resolve(
+          result
+)}
+ },
+    fields: [
+        {
+            name: "string",
+            description: "The string to count arguments for.",
+            required: true,
+            type: "STRING"
+        }
+    ]
+}
+
+module.exports = func
